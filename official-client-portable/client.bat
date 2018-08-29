540169f61aa0b312d485dc559360a08d06cdc889
hunterwb <hunter@hunterwb.com>
2018-08-29 16:33:55 -0700

    Improve client.sh

--- official-client-portable/client.bat
+++ official-client-portable/client.bat
@@ -4,19 +4,19 @@ set _JAVA_COMMAND=java
 
 cd "%~dp0"
 
-for %%I in (.) do set _DIR_NAME="%%~nxI"
+for %%I in (.) do set _DIR_BASENAME="%%~nxI"
 
 %_JAVA_COMMAND% --version || goto end
 
-if not exist cache\ mkdir cache || goto end
-
 powershell -Command "Invoke-WebRequest -OutFile jagexappletviewer.jar http://www.runescape.com/downloads/jagexappletviewer.jar" || goto end
+
+if not exist cache\ mkdir cache || goto end
 
 %_JAVA_COMMAND% -jar^
  -Duser.home=cache^
  -Dsun.awt.noerasebackground=true^
  -Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws^
- jagexappletviewer.jar %_DIR_NAME% || goto end
+ jagexappletviewer.jar %_DIR_BASENAME% || goto end
 
 del jagexappletviewer.jar
 
--- official-client-portable/client.sh
+++ official-client-portable/client.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+set -e
+
+_JAVA_COMMAND=java
+
+cd "$(dirname "$BASH_SOURCE")"
+
+_DIR_BASENAME="$(basename "$(pwd)")"
+
+$_JAVA_COMMAND --version
+
 curl http://www.runescape.com/downloads/jagexappletviewer.jar -O
-java -jar -Duser.home="$(dirname "$BASH_SOURCE")" -Dsun.awt.noerasebackground=true -Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws jagexappletviewer.jar official-client-portable
+
+mkdir -p cache
+
+$_JAVA_COMMAND -jar \
+ -Duser.home=cache \
+ -Dsun.awt.noerasebackground=true \
+ -Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws \
+ jagexappletviewer.jar $_DIR_BASENAME
+
 rm jagexappletviewer.jar
