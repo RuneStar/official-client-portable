bd801c92b66a808636f4341d580f2d24bc5fed03
hunterwb <hunter@hunterwb.com>
2018-10-24 15:29:52 -0700

    Update scripts

--- client.bat
+++ client.bat
@@ -1,22 +1,66 @@
 @echo off
 
-set _JAVA_COMMAND=java
-
 cd "%~dp0"
 
-for %%I in (.) do set _DIR_BASENAME="%%~nxI"
+set _arch=x64
+set sys_32=%SYSTEMROOT%\system32
+if exist %sys_32%\reg.exe (
+    %sys_32%\reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | %sys_32%\find /i "x86" > NUL && set arch_ext=x32
+) else (
+    if "%PROCESSOR_ARCHITECTURE%" == "x86" set arch_ext=x32
+)
 
-%_JAVA_COMMAND% --version || goto end
+set _java_version=11
 
-powershell -Command "Invoke-WebRequest -OutFile jagexappletviewer.jar http://www.runescape.com/downloads/jagexappletviewer.jar" || goto end
+set _jre_dir=jre-%_java_version%-windows-%_arch%
 
-if not exist cache\ mkdir cache || goto end
+if not exist %_jre_dir%\ (
+	set _temp_dir=temp
+	if not exist %_temp_dir%\ mkdir %_temp_dir%
+	
+	set _temp_jdk_archive=%_temp_dir%\jdk-%_java_version%-windows-%_arch%-archive.zip
+	
+	powershell -Command^
+	 "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;"^
+	 "$ProgressPreference = 'SilentlyContinue';"^
+	 "Invoke-WebRequest -OutFile '%_temp_jdk_archive%' 'https://api.adoptopenjdk.net/v2/binary/releases/openjdk%_java_version%?openjdk_impl=hotspot&release=latest&type=jdk&heap_size=normal&os=windows&arch=%_arch%';" || goto end
+	
+	set _temp_jdk_dir=%_temp_dir%\jdk-%_java_version%-windows-%_arch%
+	if not exist %_temp_jdk_dir%\ mkdir %_temp_jdk_dir%
+	
+	powershell -Command^
+	 "$shell = New-Object -COM Shell.Application;"^
+	 "$target = $shell.NameSpace((Resolve-Path .\\%_temp_jdk_dir%\).Path);"^
+	 "$zip = $shell.NameSpace((Resolve-Path .\\%_temp_jdk_archive%).Path);"^
+	 "$target.CopyHere($zip.Items(), 16);" || goto end
+	
+	for /f %%i in ('dir /b %_temp_jdk_dir%') do set _jdk_home=%_temp_jdk_dir%\%%i\
+	
+	%_jdk_home%\bin\jlink -v^
+ 	 --no-header-files^
+	 --no-man-pages^
+	 --strip-debug^
+	 --compress=2^
+	 --module-path %_jdk_home%\jmods^
+	 --add-modules java.desktop,java.management^
+	 --output %_jre_dir% || goto end
+	 
+	rmdir /s /q %_temp_dir%
+)
 
-%_JAVA_COMMAND% -jar^
+powershell -Command^
+ "$ProgressPreference = 'SilentlyContinue';"^
+ "Invoke-WebRequest -OutFile jagexappletviewer.jar http://www.runescape.com/downloads/jagexappletviewer.jar;" || goto end
+
+if not exist cache\ mkdir cache
+
+for %%I in (.) do set _dir_basename="%%~nxI"
+
+%_jre_dir%\bin\java -jar^
  -Duser.home=cache^
  -Dsun.awt.noerasebackground=true^
  -Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws^
- jagexappletviewer.jar %_DIR_BASENAME% || goto end
+ jagexappletviewer.jar %_dir_basename% || goto end
 
 del jagexappletviewer.jar
 
--- client.command
+++ client.command
@@ -16,50 +16,48 @@ case $_arch in
 	i386|i686) _arch=x32 ;;
 esac
 
-_jre_dir="jre-$_os-$_arch/"
+_java_version="11"
+
+_jre_dir="jre-$_java_version-$_os-$_arch/"
 
 if [ ! -d "$_jre_dir" ]; then
-	_jdk_dir="jdk-$_os-$_arch/"
-	_temp_jdk_dl="temp-jdk-dl"
+	_temp_dir="temp/"
+	mkdir -p "$_temp_dir"
 	
-	curl -Lfo "$_temp_jdk_dl" "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?openjdk_impl=hotspot&release=latest&type=jdk&heap_size=normal&os=$_os&arch=$_arch"
+	_temp_jdk_archive="$_temp_dir/jdk-$_java_version-$_os-$_arch-archive"
+	curl -Lfo "$_temp_jdk_archive" "https://api.adoptopenjdk.net/v2/binary/releases/openjdk$_java_version?openjdk_impl=hotspot&release=latest&type=jdk&heap_size=normal&os=$_os&arch=$_arch"
+	
+	_temp_jdk_dir="$_temp_dir/jdk-$_java_version-$_os-$_arch/"
+	mkdir -p "$_temp_jdk_dir"
 	
 	if [ "$_os" = "windows" ]; then
-		_temp_jdk_dir="temp-jdk-dir/"
-		unzip -d "$_temp_jdk_dir" "$_temp_jdk_dl"
-		cp -r "$_temp_jdk_dir/$(ls $_temp_jdk_dir)" "$_jdk_dir"
-		rm -rfv "$_temp_jdk_dir"
+		unzip -d "$_temp_jdk_dir" "$_temp_jdk_archive"
 	else
-		mkdir -p "$_jdk_dir"
-		tar -zxf "$_temp_jdk_dl" --strip-components=2 -C "$_jdk_dir"
+		tar -zxf "$_temp_jdk_archive" --strip-components=1 -C "$_temp_jdk_dir"
 	fi
 	
-	rm -v "$_temp_jdk_dl"
+	_jdk_home="$_temp_jdk_dir/$(ls $_temp_jdk_dir)"
 	
-	"$_jdk_dir/bin/jlink" \
-	 -v \
+	"$_jdk_home/bin/jlink" -v \
  	 --no-header-files \
 	 --no-man-pages \
 	 --strip-debug \
 	 --compress=2 \
-	 --module-path "$_jdk_dir\jmods" \
+	 --module-path "$_jdk_home\jmods" \
 	 --add-modules java.desktop,java.management \
 	 --output "$_jre_dir"
 	 
-	rm -rfv "$_jdk_dir"
+	rm -rfv "$_temp_dir"
 fi
-
-"$_jre_dir/bin/java" --version
 
 curl -fO http://www.runescape.com/downloads/jagexappletviewer.jar
 
 mkdir -p cache
 
-"$_jre_dir/bin/java" \
- -jar \
+"$_jre_dir/bin/java" -jar \
  -Duser.home=cache \
  -Dsun.awt.noerasebackground=true \
  -Dcom.jagex.configuri=jagex-jav://oldschool.runescape.com/jav_config.ws \
  jagexappletviewer.jar "$(basename "$(pwd)")"
-
+ 
 rm -v jagexappletviewer.jar
